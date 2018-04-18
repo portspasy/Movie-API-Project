0af1e32327b2f20e5fe44ae432ca6ba0029ec3f0
portspasy <t.culi91@gmail.com>
2018-04-18 17:38:33 +0200

    3rd commit

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function getMovies(searchText) {
                 output += `
                     <div class="col-md-3">
                         <div class="well text-center">
-                            <img src="${movie.Poster}" alt="">
+                            <img src="${movie.Poster}">
                             <h5>${movie.Title}</h5>
                             <a onclick="movieSelected("${movie.imdbID}")" class="btn btn-primary" href="#">Movie Details</a>
                         </div>
@@ -27,6 +27,56 @@ function getMovies(searchText) {
             });
 
             $("#movies").html(output);
+        })
+
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function movieSelected(id) {
+    sessionStorage.setItem("movieId", id);
+    window.location = "movie.html";
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.setItem("movieId");
+
+    axios.get("http://www.omdbapi.com?i=" +movieId)
+        .then((response) => {
+            console.log(response);
+            let movies = response.data;
+            let output = `
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    <div class="col-md-6">
+                        <div class="well text-center">
+                            <h2>${movie.Title}</h2>
+                            <ul class="list-group">
+                                <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                                <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                                <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                                <li class="list-group-item"><strong>Imdb Rating:</strong> ${movie.imdbRating}</li>
+                                <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                                <li class="list-group-item"><strong>Writter:</strong> ${movie.Writter}</li>
+                                <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <h3>plot</h3>
+                        ${movie.Plot}
+                        <hr>
+                        <a href="http"//imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                        <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                    </div>
+                </div>
+            `;
         })
 
         .catch((err) => {
